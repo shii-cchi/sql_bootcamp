26e52b6d49d8b5b84d72079cae596708b0285c18
Shion <umaradri@student.21-school.ru>
2023-08-30 20:02:40 +0700

    change model

--- src/day06/model.sql
+++ src/day06/model.sql
@@ -186,3 +186,9 @@ WHERE order_date = '2022-02-25';
 
 DELETE FROM menu
 WHERE pizza_name = 'greek pizza';
+
+INSERT INTO person_visits (id, person_id, pizzeria_id, visit_date)
+VALUES ((SELECT(MAX(id) + 1) FROM person_visits),
+        (SELECT id FROM person WHERE name = 'Dmitriy'),
+        (SELECT id FROM pizzeria WHERE name = 'DoDo Pizza'),
+        '2022-01-08');
